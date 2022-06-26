19e19baae45cde19583d556b60c07fd5bd914919
LIU42 <3528865430@qq.com>
2022-06-26 22:45:20 +0800

    少量修改。

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char* argv[])
 {
     Uint32 startTick;
     Uint32 endTick;
-    Uint32 delta;
+    Uint32 delayTick;
 
     window.init();
     window.loadImage();
@@ -30,8 +30,8 @@ int main(int argc, char* argv[])
         game.display();
 
         endTick = SDL_GetTicks();
-        delta = (1000 / GAME_FPS) - (endTick - startTick);
-        if (delta >= 0 && delta <= 1000 / GAME_FPS) { SDL_Delay(delta); }
+        delayTick = (1000 / GAME_FPS) - (endTick - startTick);
+        if (delayTick <= 1000 / GAME_FPS) { SDL_Delay(delayTick); }
     }
     window.close();
     return 0;
